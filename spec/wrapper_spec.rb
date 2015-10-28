452f94491593c1e0d0c9b7699a36b1e5fbe3d36d
Scott Moe <scott.moe@cloudspace.com>
2015-10-28 17:00:03 -0400

    Resolve effect parameter issues
    Add tests for several effect chain examples

--- spec/wrapper_spec.rb
+++ spec/wrapper_spec.rb
@@ -1,4 +1,4 @@
-require 'libsox'
+require 'ruby_libsox'
 
 describe 'LibSoX' do
   before :all do
@@ -42,6 +42,51 @@ describe 'LibSoX' do
       expect(output.signal.precision).to eq  input.signal.precision
       expect(output.signal.channels).to eq input.signal.channels
       expect(output.signal.length).to eq input.signal.length
+    end
+  end
+
+  describe 'effects chain' do
+    it 'can create an effects chain' do
+      input = LibSoX.open_read './spec/fixtures/good_bad_ugly.wav'
+      output = LibSoX.open_write './tmp/output.wav', input.signal
+      result = LibSoX.new_chain input, output
+      expect(result).to be_a Chain
+    end
+
+    it 'will throw an exception when an effect cannot be found' do
+      input = LibSoX.open_read './spec/fixtures/good_bad_ugly.wav'
+      output = LibSoX.open_write './tmp/output.wav', input.signal
+      chain = LibSoX.new_chain input, output
+      expect { chain.add_effect('raw') }.to raise_error "Effect: raw not found"
+    end
+
+    it 'will copy a file' do
+      input = LibSoX.open_read './spec/fixtures/good_bad_ugly.wav'
+      output = LibSoX.open_write './tmp/output.wav', input.signal
+      chain = LibSoX.new_chain input, output
+      chain.add_effect('input')
+      chain.add_effect('output')
+      expect( chain.flow ).to eq 0
+    end
+
+    it 'will apply an effect' do
+      input = LibSoX.open_read './spec/fixtures/good_bad_ugly.wav'
+      output = LibSoX.open_write './tmp/output.wav', input.signal
+      chain = LibSoX.new_chain input, output
+      chain.add_effect('input')
+      chain.add_effect('flanger')
+      chain.add_effect('output')
+      expect( chain.flow ).to eq 0
+    end
+
+    it 'will do an effect with many parameters' do
+      input = LibSoX.open_read './spec/fixtures/make_my_day.wav'
+      output = LibSoX.open_write './tmp/output.wav', input.signal
+      chain = LibSoX.new_chain input, output
+      chain.add_effect('input')
+      chain.add_effect('echos', '0.8', '0.7', '40', '0.25', '63', '0.3')
+      chain.add_effect('output')
+      expect( chain.flow ).to eq 0
     end
   end
 end
